59063e6f1b759ee542c7a07171518c8c244e5022
Hyunjae Sung <jae_k@hotmail.com>
2020-11-24 16:24:49 +0900

    should decode not encode

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 def convString(kor):
-    return kor.encode("euc-kr").encode("utf-8","backslashreplace")
+    return kor.encode("euc-kr").decode("utf-8","backslashreplace").replace("x","'")
 
 if __name__ == '__main__':
     print(convString(sys.argv[1]))
